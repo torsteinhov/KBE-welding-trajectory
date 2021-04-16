e108903cc79d0dcfae09b35da88c54b80ab1286d
aaKolstad <hilde8622@hotmail.com>
2021-04-16 08:55:33 +0200

    working on removing lines

--- lineSlicer.py
+++ lineSlicer.py
@@ -30,4 +30,19 @@ def findBasePlane():
     print("len(baseplan): ", len(basePlane))
     return basePlane
 
-findBasePlane()
+testPlane = findBasePlane()
+lineNumberIndex = 0
+x-val = 0
+for i, line in enumerate(testPlane):
+    print("Line: ", line)
+    print("points: ", line[0], line[1])
+    print("type point: ", type(str(line[0])))
+    print("x-val: ", line[0][0])
+
+    for point in line:
+        strPoint = str(line[0])
+        # finn x-verdi
+        #hvis x>x-val
+            #lineNumberIndex=i
+
+    # finn 
